cede56f0eb2dc3c56edac573cc8c63fccc95e69d
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2023-04-12 15:31:53 +0900

    lesson8

--- week01/src/remember/Sample.java
+++ week01/src/remember/Sample.java
@@ -13,6 +13,8 @@ public class Sample {
         sample.printNot3Codes2();
         sample.printNot23Codes1();
         sample.printNot23Codes2();
+        System.out.println(sample.code2num(14));
+        System.out.println(sample.code2suit(50));
     }
     void hello() {
         System.out.println("Hello world");
@@ -73,7 +75,8 @@ public class Sample {
             }
         }
         System.out.println();
-    }void printNot23Codes2() {
+    }
+    void printNot23Codes2() {
         for(int i = 1; i <= 52; i++) {
             if(!((i % 2 == 0) || (i % 3 == 0))) {
                 System.out.print(i + " ");
@@ -81,5 +84,27 @@ public class Sample {
         }
         System.out.println();
     }
+
+    int code2num(int code) {
+        int num = 0;
+        num = (code - 1) % 13;
+        return num + 1;
+    }
+    String code2suit(int code) {
+        int id = 0;
+        switch(code / 13) {
+            case 0:
+                return "スペド";
+            case 1:
+                return "ダイヤ";
+            case 2:
+                return "ハード";
+            case 3:
+                return "クラブ";
+            default:
+                return "";
+        }
+    }
+
 
 }
