d8363b66755c75d4b02e4482c6f47aacc9a8ce3b
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2023-04-12 14:22:00 +0900

    lesson3

--- week01/src/remember/Sample.java
+++ week01/src/remember/Sample.java
@@ -1,15 +1,22 @@
 package remember;
 
 public class Sample {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Sample sample = new Sample();
         sample.hello();
         sample.morning("SATOSHI");
+        sample.printAllCodes();
     }
-    void hello(){
+    void hello() {
         System.out.println("Hello world");
     }
-    void morning(String name){
+    void morning(String name) {
         System.out.println("Good morning " + name + "!");
+    }
+    void printAllCodes() {
+        for(int i=1; i <= 52; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
