ce5b86a9c771383ce6625cf26fb4b25a6151d7e8
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2023-04-19 14:53:43 +0900

    kadai 3-1

---
+++ week01/src/remember/CardDeck.java
@@ -0,0 +1,57 @@
+package remember;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardDeck {
+    List<Integer> cards;
+
+    public CardDeck() {
+        cards = new ArrayList<>();
+        cards.add(1);
+        cards.add(15);
+        cards.add(31);
+        cards.add(49);
+        cards.add(52);
+    }
+
+    public void print() {
+        for(int i =0; i < cards.size(); i++) {
+            int c = cards.get(i);
+            System.out.print(c + " ");
+        }
+        System.out.println();
+    }
+
+    public int getSize() {
+        return 0;
+    }
+
+    public int getCodeAt(int x) {
+        return 0;
+    }
+
+    private static String code2label(int code) {
+        return "" + code;
+    }
+
+    public void shuffle() {
+
+    }
+
+    public int takeCard() {
+        return 0;
+    }
+
+    public static void main(String args[]) {
+        CardDeck deck = new CardDeck();
+        System.out.println(deck.getCodeAt(3));
+        System.out.println(deck.getSize());
+        int c1 = deck.getCodeAt(0);
+        System.out.println(code2label(c1));
+        int c2 = deck.getCodeAt(2);
+        System.out.println(code2label(c2));
+        deck.print();
+    }
+}
