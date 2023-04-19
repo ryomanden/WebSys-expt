f09818740158d7caa3f5da2951aa5f9e1d09551e
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2023-04-19 13:48:05 +0900

    create ArraySample

---
+++ week01/src/remember/ArraySample.java
@@ -0,0 +1,58 @@
+package remember;
+
+public class ArraySample {
+    int prime[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+    int data[];
+
+    public static void main(String args[]) {
+        ArraySample sample = new ArraySample();
+
+        sample.initData();
+        sample.printData();
+        sample.printData2();
+
+        sample.swap(0, 4);
+        sample.printData();
+
+        sample.primesum();
+
+        int r = (int)(Math.random() * 5);
+        System.out.println(r);
+    }
+
+    void initData() {
+        data = new int[prime.length];
+        for(int i = 0; i < prime.length; i++) {
+            data[i] = prime[i];
+        }
+    }
+
+    void printData() {
+        for(int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+
+    }
+
+    void printData2() {
+        for(int d: data) {
+            System.out.print(d + " ");
+        }
+        System.out.println();
+    }
+
+    void swap(int x, int y) {
+        int temp = data[x];
+        data[x] = data[y];
+        data[y] = temp;
+    }
+
+    void primesum() {
+        int ps[] = new int[0];
+    }
+
+    void shuffle() {
+
+    }
+}
