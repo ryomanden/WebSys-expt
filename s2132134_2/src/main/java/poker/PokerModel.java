dbc6817de7d5c34cdc5fa2f30cefe09046fdcbcd
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2023-05-10 14:30:47 +0900

    kadai2

--- s2132134_2/src/main/java/poker/PokerModel.java
+++ s2132134_2/src/main/java/poker/PokerModel.java
@@ -1,20 +1,94 @@
 package poker;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PokerModel {
-/** ゲーム回数 */
-int games;
-/** コンストラクタ */
-public PokerModel() {
-}
-/** 一連のゲームを開始する */
-public void reset() {
-games = 0;
-}
-/** 次のゲームのためにカードを配りなおす */
-public void nextgame() {
-games++;
-}
-/** JSPから呼び出されるメソッド */
-public int getGames() {
-return games;
-}
+	/** ゲーム回数 */
+	int games;
+	/** コンストラクタ */
+	public PokerModel() {
+
+		deckcards = new ArrayList<>();
+		handcards = new ArrayList<>();
+	}
+	/** 一連のゲームを開始する */
+	public void reset() {
+		games = 0;
+		chips = 500;
+	}
+	/** 次のゲームのためにカードを配りなおす */
+	public void nextgame() {
+		// 52枚の山札を作る
+		deckcards.clear();
+		for (int i=1; i<=52; i++) {
+			deckcards.add(i);
+		}
+		// 山札の先頭から5枚抜いて手札にする
+		handcards.clear();
+		for (int i=0; i<5; i++) {
+			int n = deckcards.remove(0);
+			handcards.add(n);
+		}
+		// 残りの場札の枚数とカード番号をコンソールに表示する
+		System.out.printf("%d: ", deckcards.size());
+		for (int n: deckcards) {
+			System.out.printf("%d ", n);
+		}
+		System.out.println();
+		message = "交換するカードをチェックしてください";
+		buttonLabel = "交換";
+		games++;
+	}
+
+	public void change(List<String> index) {
+		System.out.println("index="+index);
+		for (int i=0; i<5; i++) {
+			int c = deckcards.remove(0); // 山札の先頭を取り除き、
+			handcards.set(i, c); // 手札の指定場所に入れる
+		}
+		evaluate();
+		buttonLabel = "次のゲーム";
+	}
+	/** 役の判別を行い、チップを増減させる */
+	public void evaluate() {
+		chips -= 100;
+		message = "ノーペアです -100";
+		if (chips == 0) {
+			message += " ゲームオーバー";
+		}
+	}
+
+	/** JSPから呼び出されるメソッド */
+	public int getGames() {
+		return games;
+	}
+
+	/** 現在のチップ数 初期値は500 */
+	int chips;
+	/** 山札 */
+	List<Integer> deckcards;
+	/** 手札 */
+	List<Integer> handcards;
+	/** 送信ボタンに表示する文字列("交換","次のゲーム"のどちらか */
+	String buttonLabel;
+	/** プレイヤーへのメッセージ */
+	String message;
+
+	/** 現在のチップ数を返す */
+	public int getChips() {
+		return chips;
+	}
+	/** 5枚の手札のうち，i番目のカード番号を返す (i=0～4) */
+	public int getHandcardAt(int i) {
+		return handcards.get(i);
+	}
+	/** 送信ボタンのラベルを返す */
+	public String getButtonLabel() {
+		return buttonLabel;
+	}
+	/** プレイヤーへのメッセージを返す */
+	public String getMessage() {
+		return message;
+	}
 }
--- s2132134_2/src/main/webapp/poker.jsp
+++ s2132134_2/src/main/webapp/poker.jsp
@@ -3,6 +3,7 @@ pageEncoding="UTF-8"%>
 <%@ page import="poker.PokerModel" %>
 <%
 PokerModel model = (PokerModel)request.getAttribute("model");
+String label = model.getButtonLabel();
 %>
 <!DOCTYPE html>
 <html>
@@ -14,17 +15,30 @@ PokerModel model = (PokerModel)request.getAttribute("model");
 ポーカーゲーム
 <hr>
 ゲーム回数：<%= model.getGames() %>
+<br>
+チップ：<%= model.getChips() %>
 <hr>
+<%= model.getMessage() %>
 <form action="/s2132134_2/PokerServlet" method="POST">
-<input type="submit" value="送信">
+<table>
+<tr>
+<td><img src="cards/<%= model.getHandcardAt(0) %>.png" width="100" height="150"></td>
+<td><img src="cards/<%= model.getHandcardAt(1) %>.png" width="100" height="150"></td>
+<td><img src="cards/<%= model.getHandcardAt(2) %>.png" width="100" height="150"></td>
+<td><img src="cards/<%= model.getHandcardAt(3) %>.png" width="100" height="150"></td>
+<td><img src="cards/<%= model.getHandcardAt(4) %>.png" width="100" height="150"></td>
+</tr>
+<tr align="center">
+<td><input type="checkbox" name="change" value="0"></td>
+<td><input type="checkbox" name="change" value="1"></td>
+<td><input type="checkbox" name="change" value="2"></td>
+<td><input type="checkbox" name="change" value="3"></td>
+<td><input type="checkbox" name="change" value="4"></td>
+</tr>
+</table>
+<input type="submit" value="<%= label %>">
 </form>
 <hr>
 <a href="/s2132134_2/PokerServlet">リセット</a>
-<hr>
-<% if (model.getGames() < 5) { %>
-5回未満
-<% } else { %>
-5回以上
-<% } %>
 </body>
 </html>
